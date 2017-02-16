0fe7dcb1faa347764c8d808a2c8c82811bf03d1f
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-02-16 18:19:33 +0100

    add draft of the pipeline to tools and a few functions docs

---
+++ tools/kernels.py
@@ -0,0 +1,9 @@
+def kernel(x, y, type="linear"):
+    '''
+    Args :
+           - x ndarray (., d): sample image
+           - y ndarray (., d): sample image
+           - type string: which type of kernel you want to derive
+    Returns :
+             - float
+    '''
