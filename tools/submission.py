5a32fb163e7f45b117afbe15b772f204cb5cf758
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-02-16 19:48:27 +0100

    add submission formatting function

---
+++ tools/submission.py
@@ -0,0 +1,27 @@
+import os.path as op
+
+import numpy as np
+import pandas as pd
+
+
+def labels_to_csv(Y, kernel="linear", algo="linear regression", user="Zac",
+                  file_name=None):
+    '''Saves the labels in the correct format.
+        Args:
+            - Y (ndarray): the labels.
+            - kernel (str): the type of kernel used.
+            - algo (str): the algorithm used.
+            - user (str): who you are.
+            - file_name (str): overwrites the file name.
+    '''
+    df_labels = pd.DataFrame(data=Y,
+                             index=np.arange(len(Y))+1,
+                             columns=["Prediction"])
+    df_labels.index.names = ["Id"]
+    if file_name is None:
+        file_name = "Yte_{kernel}_{algo}_{user}.csv".format(
+            kernel=kernel,
+            algo=algo,
+            user=user
+        )
+    df_labels.to_csv(op.join("submissions", file_name))
