4e99e7b1c5f47126951c54cc2f065d18c0f27291
tournemire <pierredet@gmail.com>
2017-02-17 09:42:29 +0100

    basic loading functions

--- tools/data_loading.py
+++ tools/data_loading.py
@@ -1,4 +1,27 @@
-T_tr = np.loadtxt("Ytr.csv", skiprows=1, usecols=(1,), delimiter=',')
+import numpy as np
+
+
+def load_labels():
+    '''
+    load labels as a nd array ou unsigned int of 8 bits
+    '''
+    Y = np.loadtxt("../data/Ytr.csv",
+                   skiprows=1, usecols=(1,), delimiter=',').astype('uint8')
+    return Y
+
+
+def dummy_code(Y):
+    '''
+    Args :
+           - type nd array  : Labels as a vector of float
+    Returns :
+           - ndarray (., n_classes) labels dummy coded
+    '''
+    n = Y.shape[0]
+    Y_dum = np.zeros((n, int(Y.max())+1))
+    Y_dum[np.arange(n), Y.astype('uint8')] = 1
+    return Y_dum
+
 
 def load_images(type="train"):
     '''
@@ -7,3 +30,19 @@ def load_images(type="train"):
     Returns :
            - X ndarray (5000,3072)
     '''
+    if type == "train":
+        path = '../data/Xtr.csv'
+    elif type == "test":
+        path = '../data/Xte.csv'
+    else:
+        print("Type Error : argument should be either 'train' or 'test'")
+        return
+
+    f = open(path)
+    lines = f.readlines()
+    X = np.empty((len(lines), 3072), dtype=np.float32)
+
+    for i, line in enumerate(lines):
+        X[i] = np.fromstring(line[:line.index(',\n')], dtype=np.float32,
+                             sep=",")
+    return X
