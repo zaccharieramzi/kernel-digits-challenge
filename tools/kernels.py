715b2bb05b16a46cb04c5b96d62ec7c19df3d401
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-02-18 14:25:41 +0100

    changed type arg name to specific name in order not to take reserved name

--- tools/kernels.py
+++ tools/kernels.py
@@ -1,27 +1,28 @@
 import numpy as np
 
 
-def kernel(x, y, type="linear", **kwargs):
+def kernel(x, y, kernel_type="linear", **kwargs):
     '''
     Args :
            - x ndarray (., d): sample image.
            - y ndarray (., d): sample image.
-           - type string: which type of kernel you want to derive.
+           - kernel_type string: which type of kernel you want to derive.
            - **kwargs: arguments to be passed on to a specific kernel type.
     Returns :
              - float: the value of k(x,y)
     '''
-    if type == "linear":
+    if kernel_type == "linear":
         return np.dot(x, y)
     else:
-        raise ValueError("The {} kernel is not implemented".format(type))
+        raise ValueError("The {} kernel is not implemented".format(
+            kernel_type))
 
 
-def kernel_matrix(X, type="linear", **kwargs):
+def kernel_matrix(X, kernel_type="linear", **kwargs):
     '''Computes the kernel matrix for the input data X.
         Arguments:
             - X (ndarray): the input data.
-            - type (str): the type of kernel you want to use.
+            - kernel_type (str): the type of kernel you want to use.
             - **kwargs: arguments to be passed on to a specific kernel type.
         Returns:
             - ndarray: the kernel matrix.
@@ -30,6 +31,6 @@ def kernel_matrix(X, type="linear", **kwargs):
     K = np.zeros((n_data, n_data))
     for i in range(n_data):
         for j in range(i+1):
-            K[i, j] = kernel(X[i, :], X[j, :], type=type, **kwargs)
+            K[i, j] = kernel(X[i, :], X[j, :], kernel_type=kernel_type, **kwargs)
             K[j, i] = K[i, j]
     return K
