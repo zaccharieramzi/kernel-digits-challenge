9f1f14adac74fb361acff0ba60dba5a331c33376
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-02-18 14:25:41 +0100

    changed type arg name to specific name in order not to take reserved name

--- tools/optimization.py
+++ tools/optimization.py
@@ -1,10 +1,14 @@
-def find_f(K, Y, type="linear regression", **kwargs):
+def find_f(K, Y, prob_type="linear regression", **kwargs):
     '''
     Args :
            - K ndarray (., .): the kernel matrix
            - Y ndarray (.,): the labels (0 or 1)
-           - type: which type of classification problem you want to solve
+           - prob_type: which type of classification problem you want to solve
            - **kwargs: arguments to be passed to the optimization solver
     Returns :
              - alpha
     '''
+    if prob_type=="linear regression":
+
+    else:
+        raise ValueError("{} is not implemented.".format(prob_type))
