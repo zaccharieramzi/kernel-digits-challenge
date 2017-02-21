ccd3337e7a52a485ddb1d45aa2bec8312e59cf8d
pdubreuil1 <pierre.dubreuil@telecom-paristech.fr>
2017-02-21 19:42:01 +0100

    added doc

--- tools/discretization.py
+++ tools/discretization.py
@@ -2,7 +2,14 @@ import numpy as np
 
 
 def discretize_orientation(patch_x, patch_y):
-
+    '''
+    args :
+        - patch_x: gradient in x of the patch in (x, y)
+        - patch_y: gradient in y of the patch in (x, y)
+    return
+        - discrete_or: an array containing the orientation discretized between
+        0 and 15*pi/8
+    '''
     a = np.arctan2(patch_y, patch_x)/(2*np.pi)
     a = round(a*8)/8*(2*np.pi)
     rounded_patch_x = np.cos(a)
@@ -13,6 +20,13 @@ def discretize_orientation(patch_x, patch_y):
 
 
 def pin_as_vect(discrete_or):
+    '''
+    Converting the orientation matrix to a pin (to be used in kmeans)
+    args :
+        - discrete_or: pin as a matrix patch_size*patch_size
+    return
+        - vect: pin as a vector counting orientation occurences
+    '''
     vect = np.zeros(16)
     for row in discrete_or:
         for el in row:
