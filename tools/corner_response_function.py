38ecd97ec98c6e5eec7f66d5e1aec8fd43b7e921
tournemire <pierredet@gmail.com>
2017-02-21 18:32:26 +0100

    fix various bugs in edge and corner detection and add visualization tools

--- tools/corner_response_function.py
+++ tools/corner_response_function.py
@@ -15,7 +15,7 @@ def compute_corner_response(I_x, I_y):
     M[1, 0] = M[0, 1]
     M[1, 1] = np.square(I_y).sum()
 
-    R = M[0, 0] * M[1, 1] - M[1, 0] * M[0, 1] - 0.05 * (M[0, 0] + M[1, 1])
+    R = M[0, 0] * M[1, 1] - M[1, 0] * M[0, 1] - 0.05 * (M[0, 0] + M[1, 1])**2
 
     return R
 
@@ -35,7 +35,21 @@ def gaussian_filter_2d(shape=(3, 3), sigma=0.5):
     return h
 
 
-GAUSSIAN_FILTER = gaussian_filter_2d()
+def grad(im):
+    fx = np.zeros_like(im)
+    fy = np.zeros_like(im)
+    fx[:-1, :] = im[1:, :] - im[:-1, :]
+    # fx[-1, :] = 0 redundant
+
+    fy[:, :-1] = im[:, 1:] - im[:, :-1]
+    # fy[:, -1] = 0 redundant
+    return fx, fy
+
+
+FILTERX, FILTERY = grad(gaussian_filter_2d(shape=(5, 5)))
+# we crop to avoid border effects
+FILTERX = FILTERX[1:-1, 1:-1]
+FILTERY = FILTERY[1:-1, 1:-1]
 
 
 def compute_gaussian_grad(image_mat):
@@ -49,38 +63,34 @@ def compute_gaussian_grad(image_mat):
     image_grad_x = np.zeros_like(image_mat)
     image_grad_y = np.zeros_like(image_mat)
 
-    def grad(im):
-        fx = np.zeros_like(im)
-        fy = np.zeros_like(im)
-        fx[:-1, :] = im[1:, :] - im[:-1, :]
-        # fx[-1, :] = 0 redundant
-
-        fy[:, :-1] = im[:, 1:] - im[:, :-1]
-        # fy[:, -1] = 0 redundant
-        return fx, fy
-
-    filterx, filtery = grad(GAUSSIAN_FILTER)
-
+    # convolution
     for i in range(image_mat.shape[0]):
         for j in range(image_mat.shape[1]):
-            rmin = max(0, i - filterx.shape[0] // 2)
-            rmax = min(image_mat.shape[0], i + filterx.shape[0] // 2)
-            rmin_f = rmin - i + filterx.shape[0] // 2
-            rmax_f = rmax - i + filterx.shape[0] // 2
+            # x-axis indices for image
+            rmin = max(0, i - FILTERX.shape[0] // 2)
+            rmax = min(image_mat.shape[0], i + FILTERX.shape[0] // 2)
+            # x-axis indices for filter
+            rmin_f = rmin - i + FILTERX.shape[0] // 2
+            rmax_f = rmax - i + FILTERX.shape[0] // 2
             if rmax_f == 0:
-                rmax_f = filterx.shape[0]
+                rmax_f = FILTERX.shape[0]
 
-            smin = max(0, j - filterx.shape[1] // 2)
-            smax = min(image_mat.shape[1], j + filterx.shape[1] // 2)
-            smin_f = smin - j + filterx.shape[1] // 2
-            smax_f = smax - j + filterx.shape[1] // 2
+            # y-axis indices for image
+            smin = max(0, j - FILTERX.shape[1] // 2)
+            smax = min(image_mat.shape[1], j + FILTERX.shape[1] // 2)
+            # y-axis indices for filter
+            smin_f = smin - j + FILTERX.shape[1] // 2
+            smax_f = smax - j + FILTERX.shape[1] // 2
             if smax_f == 0:
-                smax_f = filterx.shape[1]
+                smax_f = FILTERX.shape[1]
 
             image_grad_x[i, j] = np.sum(
-                image_mat[rmin:rmax, smin:smax] * filterx[rmin_f:rmax_f,
+                image_mat[rmin:rmax, smin:smax] * FILTERX[rmin_f:rmax_f,
                                                           smin_f:smax_f])
             image_grad_y[i, j] = np.sum(
-                image_mat[rmin:rmax, smin:smax] * filtery[rmin_f:rmax_f,
+                image_mat[rmin:rmax, smin:smax] * FILTERY[rmin_f:rmax_f,
                                                           smin_f:smax_f])
+    # let's crop to avoid border effect
+    image_grad_x = image_grad_x[1:-1, 1:-1]
+    image_grad_y = image_grad_y[1:-1, 1:-1]
     return image_grad_x, image_grad_y
--- tools/feature_learning.py
+++ tools/feature_learning.py
@@ -4,12 +4,14 @@ from .corner_response_function import compute_gaussian_grad, compute_corner_resp
 from .visualization import reshape_as_images, imshow
 from .data_loading import load_images
 
+FILTER_SIZE = 3
+
 
 def feature_learning():
     X = load_images(type="test")[:100]
     image_list = reshape_as_images(X)
     image_list = image_list.mean(axis=3)
-    window_size = 3
+    window_size = 5
     stride = 3
     patch_size = 5
     pins = list()
@@ -18,33 +20,56 @@ def feature_learning():
         im_size = image_mat.shape[0]  # normally 32
         image_grad_x, image_grad_y = compute_gaussian_grad(image_mat)
 
-        R_size = (im_size - window_size)/stride + 1
+        R_size = (image_grad_x.shape[0] - window_size)//stride + 1
         R = np.zeros((R_size, R_size))
         for i in range(R_size):
             for j in range(R_size):
-                I_x = image_grad_x[i*stride:i*stride+window_size]
-                I_y = image_grad_y[j*stride:j*stride+window_size]
+                I_x = image_grad_x[i*stride:i*stride+window_size,
+                                   j*stride:j*stride+window_size]
+                I_y = image_grad_y[i*stride:i*stride+window_size,
+                                   j*stride:j*stride+window_size]
+
                 R[i, j] = compute_corner_response(I_x, I_y)
         # thresholding of R
         R = np.abs(R)
         x, y = np.where(
-            R > np.percentile(R, 85))
-        x = (x+1) * stride
-        y = (y+1) * stride
-        imshow(X[ind], points_of_interest=(x, y))
+            R > np.percentile(R, 75))
+        x, y = from_R_to_im(x, y, window_size, stride)
+
+        heatmap = R_to_heatmap(R, window_size, stride, im_size)
+        imshow(image_list[ind], points_of_interest=(y, x), heatmap=heatmap)
         continue
         threshold = 1
         R = R > threshold
         # data viz / number of point of interest
         # ex : R > np.percentile(R, 90) -> top 10%
 
-
         for x_r, y_r in np.where(R):  # r_pin are the coordinates in R of point of interest
             im_pin = from_R_to_im(r_pin, window_size, stride, im_size)
-            patch_x = image_grad_x[1]
-            patch_y = image_grad_y[1]
+            patch_x = image_grad_x
+            patch_y = image_grad_y
             angles = compute_orientation(patch_x, patch_y)
             pin = discretize_orientation(angles)
             pins.append(pin)
 
     # KMEANS
+
+
+def from_R_to_im(x, y, window_size, stride):
+    x = x * stride + (window_size - 1)//2 + (FILTER_SIZE - 1)//2
+    y = y * stride + (window_size - 1)//2 + (FILTER_SIZE - 1)//2
+
+    return (x, y)
+
+
+def R_to_heatmap(R, window_size, stride, im_size):
+    R_size = R.shape[0]
+    # create heatmap
+    heatmap = np.zeros((im_size, im_size))
+    for i in range(R_size):
+        for j in range(R_size):
+            ix, iy = from_R_to_im(i, j, window_size, stride)
+            heatmap[ix:ix+window_size,
+                    iy:iy+window_size] = R[i, j]
+    heatmap /= heatmap.max()
+    return heatmap
