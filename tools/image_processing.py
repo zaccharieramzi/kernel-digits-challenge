ce18a14dd33d808ebeea4698538986f1da9adb53
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-02-21 18:56:24 +0100

    remove duplicate file

--- tools/image_processing.py
+++
@@ -1,7 +0,0 @@
-def process_images(X):
-    '''
-    Args :
-        - X ndarray (., 3072): each row of X is an image.
-    Returns :
-        - ndarray (., d) where d is feature length, or encoding dimension
-    '''
