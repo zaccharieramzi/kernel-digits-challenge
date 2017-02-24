fd2f44724f8baf1e2389bec2fbf2eb0d99f60172
pdubreuil1 <pierre.dubreuil@telecom-paristech.fr>
2017-02-24 18:16:07 +0100

    added the logistic regression to the possibilites

--- tools/optimization.py
+++ tools/optimization.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def find_f(K, Y, prob_type="linear regression", **kwargs):
+def find_f(K, Y, prob_type="linear regression", **kwargs, n_iter=200):
     '''
     Args :
            - K ndarray (., .): the kernel matrix
@@ -20,5 +20,54 @@ def find_f(K, Y, prob_type="linear regression", **kwargs):
         else:
             n = len(Y)
             return np.linalg.solve((K + lamb*n*np.eye(n)), Y)
+    elif prob_type == "logistic regression":
+        try:
+            lamb = kwargs["lamb"]
+        except KeyError:
+            raise KeyError("You need a lamb argument when performing a \
+                           linear regression")
+        else:
+            n = len(Y)
+            Y = 2*Y - 1
+            alpha = np.zeros(n)
+            W = np.eye(n)
+            P = 1/2*np.eye(n)
+            z = np.zeros(n)
+            for iter in range(n_iter):
+                alpha = solveWKRR(K, W, z)
+                m = K.dot(alpha)
+                P = -sigma(-Y*m)
+                W = sigma(m)*sigma(-m)
+                z = m + Y/sigma(-Y*m)
+            return alpha
     else:
         raise ValueError("{} is not implemented.".format(prob_type))
+
+
+def sigma(X):
+    '''
+    sigmoid function
+    Args :
+           - X ndarray (., 1): a one dimension vector
+
+    Returns :
+             - the sigmoid of the vector
+    '''
+    return 1/(1+np.exp(-X))
+
+
+def solveWKRR(K, W, z):
+    '''
+    solve a weighted linear regression problem for the logistic regression
+    purpose
+    Args :
+           - K ndarray (., .): the kernel matrix
+           - W ndarray (., .): the weights. Here the hessian of the sigmoid
+
+    Returns :
+             - the solution alpha at each step
+    '''
+    W_sqrt = np.sqrt(W)
+    I = lamb*n*np.eye(n)
+    return W_sqrt.dot(np.linalg.solve((W_sqrt.dot(K).dot(W_sqrt) + I,
+                                      W_sqrt.dot(Y))))
