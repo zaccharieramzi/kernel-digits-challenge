672493211a5d9912271b4de8290e81c9e524e7d4
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2017-03-05 18:22:50 +0100

    added normalization for hogs

--- tools/hog.py
+++ tools/hog.py
@@ -7,7 +7,8 @@ from .discretization import discretize_orientation, pin_as_vect
 
 def hog(
         image,
-        filter_sigma=0.1, filter_shape=5, hog_cell_size=8, disc_grid=16):
+        filter_sigma=0.1, filter_shape=5, hog_cell_size=8, disc_grid=16,
+        normalize=False, block_size=2):
     X = image.mean(axis=2)
     image_size = X.shape[0]  # 32 or 63
     # gaussian filters
@@ -22,7 +23,7 @@ def hog(
         signed=False,
         disc_grid=16)
     n_cells = image_size // hog_cell_size
-    cells_vector = np.zeros((n_cells, n_cells, disc_grid//2))
+    cells_vector = np.zeros((n_cells, n_cells, disc_grid // 2))
     for i in range(n_cells):
         for j in range(n_cells):
             cells_vector[i, j, :] = pin_as_vect(
@@ -35,4 +36,14 @@ def hog(
                     j * hog_cell_size:(j + 1) * hog_cell_size
                 ],
                 disc_grid=disc_grid)
-    return cells_vector.reshape((n_cells**2, disc_grid//2))
+    if normalize:
+        for i in range(n_cells // block_size):
+            for j in range(n_cells // block_size):
+                cells_vector[
+                    i * block_size:(i + 1) * block_size,
+                    j * block_size:(j + 1) * block_size
+                ] /= np.linalg.norm(cells_vector[
+                    i * block_size:(i + 1) * block_size,
+                    j * block_size:(j + 1) * block_size
+                ])
+    return cells_vector.reshape((n_cells**2, disc_grid // 2))
