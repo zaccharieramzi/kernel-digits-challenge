4c31ffe3fd6b540fa121bba4e4381bd7fa0d0648
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2017-03-06 19:02:55 +0100

    documented process image old function

--- tools/process_images.py
+++ tools/process_images.py
@@ -1,10 +1,10 @@
-import numpy as np
 
 from tools.quantization import vf_vector
 
 
 def process_images(n, centroids, pins, pin_to_im):
-    '''Vectorize the features of each images contained in the rows of X.
+    '''Vectorize the features of each images contained in the rows of X. Legacy
+    function therefore not documented.
     '''
     # X_proc = np.zeros((n, centroids.shape[0]))
     X_proc = vf_vector(pins, centroids, pin_to_im, n)
